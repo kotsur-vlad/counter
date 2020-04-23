9964b1fd24daaf52ef4f5504c49332b9f441acac
kotsur-vlad <kotsur.vlad@gmail.com>
2020-04-23 19:08:57 +0300

    Added main app's logic. Added methods: onIncClick, onResetClick, onSetClick, changeValue, onSettingChange

--- src/App.js
+++ src/App.js
@@ -7,37 +7,86 @@ class App extends React.Component {
 
 	state = {
 		settingValues: [
-			{title: "start value: ", type: "number", min: 0, value: 9},
-			{title: "max value: ", type: "number", min: 1,  value: 11}
+			{id: 1, title: "start value: ", type: "number", min: 0, value: 0},
+			{id: 2, title: "max value: ", type: "number", min: 1, value: 1}
 		],
 		buttonsValues: [
-			{title: "SET"},
-			{title: "INC"},
-			{title: "RESET"}
+			{id: 1, title: "SET"},
+			{id: 2, title: "INC"},
+			{id: 3, title: "RESET"}
 		],
 		startValue: 0,
-		maxValue: 5,
-		counter: 0
+		maxValue: 1,
+		counter: 0,
+		disabled: false
 	};
 
 	onIncClick = () => {
 		let counter = this.state.counter;
-		this.setState({
-			counter: ++counter
-		})
+		let maxValue = this.state.maxValue;
+		if (counter !== maxValue) {
+			this.setState ({
+				counter: ++counter
+			})
+		} else {
+			this.setState ({
+				disabled: true
+			})
+		}
 	};
 
 	onResetClick = () => {
-		this.setState({
-			counter: 0
-		})
+		this.setState ({
+			counter: this.state.startValue,
+			disabled: false
+		});
 	}
+
+	onSetClick = () => {
+		this.state.settingValues.map (set => {
+			if (set.title === "start value: ") {
+				this.setState ({
+					counter: set.value,
+					startValue: set.value
+				});
+			} else if (set.title === "max value: ") {
+				this.setState ({
+					maxValue: set.value
+				});
+			}
+		});
+		this.setState ({
+			disabled: false
+		})
+
+
+		// this.setState ({
+		// 	counter: this.state.settingValues[0].value,
+		// 	startValue: this.state.settingValues[0].value,
+		// 	maxValue: this.state.settingValues[1].value,
+		// 	disabled: false
+		// })
+		// console.log(this.state.counter)
+		// console.log(this.state.startValue)
+		// console.log(this.state.maxValue)
+	};
+
+	changeValue = (settingId, newValue) => {
+		let newSettings = this.state.settingValues.map (set => {
+			if (set.id === settingId) {
+				return {...set, value: +newValue}
+			}
+			return set;
+		});
+		this.setState ({
+			settingValues: newSettings
+		});
+	};
 
 	render = () => {
 
-		let settings = this.state.settingValues.map( val => {
-			return <ValueSetting settingValues={val}/>
-		});
+		let settings = this.state.settingValues.map (set => <ValueSetting changeValue={this.changeValue}
+																		  settingValues={set}/>);
 
 		return (
 			<div className="App">
@@ -49,7 +98,7 @@ class App extends React.Component {
 					</div>
 
 					<div className="SETUP-BUTTON">
-						<button>SET</button>
+						<button onClick={this.onSetClick}>SET</button>
 					</div>
 
 				</div>
@@ -62,7 +111,7 @@ class App extends React.Component {
 					</div>
 
 					<div className="CONTROL-BUTTONS">
-						<button onClick={this.onIncClick}>INC</button>
+						<button onClick={this.onIncClick} disabled={this.state.disabled}>INC</button>
 						<button onClick={this.onResetClick}>RESET</button>
 					</div>
 
--- src/components/ValueSetting.js
+++ src/components/ValueSetting.js
@@ -3,11 +3,17 @@ import React from 'react';
 import './../App.css';
 
 class ValueSetting extends React.Component {
+
+	onSettingChange = (e) => {
+		this.props.changeValue (this.props.settingValues.id, e.currentTarget.value);
+	};
+
 	render = () => {
 		return (
 			<div className="">
 				<span>{this.props.settingValues.title}</span>
-				<input type={this.props.settingValues.type} min={this.props.settingValues.min} value={this.props.settingValues.value}/>
+				<input type={this.props.settingValues.type} min={this.props.settingValues.min}
+					   value={this.props.settingValues.value} onChange={this.onSettingChange}/>
 			</div>
 
 		);
